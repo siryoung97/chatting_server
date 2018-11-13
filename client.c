76cf4943fa825169b2632e62dc1ee8ff781e1e57
siryoung97 <siryoung97@gmail.com>
2018-11-13 16:13:09 +0900

    concurrent_server incomplete

--- client.c
+++ client.c
@@ -5,57 +5,18 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <pthread.h>
-
-#define buffersize 100
-
-void *receive_message(void *arg);
-
-void *send_message(void *arg);
-
-int main() {
+int main(){
     int sock;
     struct sockaddr_in server_address;
-    pthread_t send_thread, receive_thread;
-
+    sock=socket(AF_INET,SOCK_STREAM,0);
     bzero(&server_address, sizeof(server_address));
-    server_address.sin_family = AF_INET;
-    server_address.sin_addr.s_addr = htons(INADDR_ANY);
-    server_address.sin_port = htonl(8080);
+    server_address.sin_family=AF_INET;
+    server_address.sin_port=htons(8080);
+    server_address.sin_addr.s_addr=htonl(INADDR_ANY);
+    connect(sock,(struct sockaddr *)&server_address, sizeof(server_address));
+    while(1){
+        
+    }
 
-    sock = socket(AF_INET, SOCK_STREAM, 0);
-
-    connect(sock, (struct sockaddr *) &server_address, sizeof(server_address));
-
-    pthread_create(&send_thread, NULL,send_message, (void *) sock);
-    pthread_create(&receive_thread, NULL,receive_message, (void *) sock);
-    close(sock);
     return 0;
-}
-
-void *send_message(void *arg) {
-    int sock = *((int *)arg);
-    char message[buffersize];
-    while (1){
-        fgets(message,buffersize,stdin);
-        if(!strcmp(message,"q\n")||strcmp(message,"q\n"))
-        {
-            close(sock);
-            exit(0);
-        }
-        write(sock,message,strlen(message));
-    }
-    return NULL;
-}
-
-void *receive_message(void *arg) {
-    int sock = *((int *)arg);
-    char message[buffersize];
-    int str_len;
-    while (1){
-        str_len=read(sock,message, sizeof(message)-1);
-        message[str_len]=0;
-        fputs(message,stdout);
-    }
-    return NULL;
 }
--- server.c
+++ server.c
@@ -5,51 +5,36 @@
 #include <arpa/inet.h>
 #include <strings.h>
 #include <unistd.h>
-#include <pthread.h>
 
-#define buffersize 100
-#define MAXCLIENT 256
-char message[buffersize];
-int client_socks[MAXCLIENT];
+int client_max = 100;
 int client_count = 0;
-
-void *handle_client(void *arg);
-
-void *sendmessage(char *message, int len);
-
-void *receivemessage(void *message);
 
 int main() {
     int server_sock, client_sock;
     struct sockaddr_in server_address, client_address;
-    int client_address_size;
-    pthread_t thread_address;
+    unsigned int client_address_size;
+    int pid;
+    server_sock = socket(AF_INET, SOCK_STREAM, 0);
     bzero(&server_address, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
     server_address.sin_port = htons(8080);
-    while (1) {
-        client_address_size = sizeof(client_address);
-        client_sock = accept(server_sock, (struct sockaddr_in *) &server_address, sizeof(server_address));
-        client_socks[client_count++] = client_sock;
-        pthread_create(&thread_address, NULL, handle_client, (void *) &client_sock);
-        pthread_detach(thread_address);
+    client_address_size = sizeof(client_address);
+    bind(server_sock, (struct sockaddr *) &server_address, sizeof(server_address));
+    listen(server_sock, client_max);
+    for (;;) {
+        client_sock = accept(server_sock, (struct sockaddr *) &server_address, &client_address_size);
+        client_count++;
+        if ((pid = fork()) == 0) {
+            if (client_sock != 0) {
+                printf("%d", client_count);
+
+            }
+            close(client_sock);
+            exit(0);
+        }
+        close(client_sock);
     }
 
+    return 0;
 }
-
-void *handle_client(void *arg) {
-    int sock = *((int *) arg);
-    while ((read(sock, message, sizeof(message))) != 0)
-        sendmessage(message, strlen(message));
-    return NULL;
-
-}
-
-void *sendmessage(char *message, int length) {
-    int i;
-    for (i = 0; i < client_count; i++)
-        write(client_socks[i], message, length);
-}
-
-
